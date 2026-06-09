b27db010c08df14045ecfadb2e6b64b44bfd35d2
Elizabeth McDaniel <elizabethmcd93@gmail.com>
2026-06-08 17:39:03 -0700

    update to figures for prepping preprint

--- scripts/zymo-oats-analysis.R
+++ scripts/zymo-oats-analysis.R
@@ -28,19 +28,53 @@ rep_mags_metadata <- mag_metadata %>%
   select(genome_accession, completeness, contamination, contigs, taxonomy, species)
 
 # sylph profiling results
-sylph_profiles <- read_tsv("results/2025-12-02-profiling/combined_sylph_profiles.tsv") %>%
+round1_sylph_profiles <- read_tsv("results/2025-12-02-profiling/combined_sylph_profiles.tsv") %>%
   mutate(accession_name = gsub("_trimmed_1.fastq.gz", "", Sample_file)) %>% 
   mutate(genome_accession = gsub(".fa", "", Genome_file)) %>% 
   select(accession_name, genome_accession, Sequence_abundance, Adjusted_ANI, Eff_cov, Contig_name)
 
+round2_sylph_profiles <- read_tsv("results/2026-01-16-profiling/combined_sylph_profiles.tsv") %>% 
+  mutate(accession_name = gsub("_trimmed_1.fastq.gz", "", Sample_file)) %>% 
+  mutate(genome_accession = gsub(".fa", "", Genome_file)) %>% 
+  select(accession_name, genome_accession, Sequence_abundance, Adjusted_ANI, Eff_cov, Contig_name)
+
+combined_sylph_profiles <- rbind(round1_sylph_profiles, round2_sylph_profiles)
+
 # sample metadata
-sample_metadata <- read.csv("metadata/2025-12-01-zymo-oat-sequencing-metadata.csv") %>% 
+round1_sample_metadata <- read.csv("metadata/2025-12-01-zymo-oat-sequencing-metadata.csv") %>% 
   mutate(accession_name = gsub("_R1.fastq.gz", "", fastq_1)) %>% 
   select(sample, accession_name, oat, day)
 
+round2_sample_metadata <- read.csv("metadata/2026-01-16-zymo-spontaneous-oats-metadata.csv") %>% 
+  mutate(accession_name = gsub("_R1.fastq.gz", "", fastq_1)) %>% 
+  mutate(accession_name = gsub(".*/", "", accession_name)) %>% 
+  select(sample, accession_name, oat, day)
+
+combined_sample_metadata <- rbind(round1_sample_metadata, round2_sample_metadata)
+
 # merge with genome and sample metadata
-sylph_profiles_metadata <- left_join(sylph_profiles, rep_mags_metadata) %>% 
-  left_join(sample_metadata)
+sylph_profiles_metadata <- left_join(combined_sylph_profiles, rep_mags_metadata) %>% 
+  left_join(combined_sample_metadata) %>% 
+  mutate(genus = str_extract(taxonomy, "[^;]+$")) 
+
+genus_map <- tibble::tribble(
+  ~pattern,                         ~genus_group,
+  "^Bacillus(_.*)?$",               "Bacillus",
+  "^Enterococcus(_.*)?$",           "Enterococcus")
+
+sylph_profiles_metadata <- sylph_profiles_metadata %>%
+  mutate(
+    genus_group = purrr::map_chr(
+      genus,
+      \(g) {
+        hit <- genus_map %>% filter(str_detect(g, pattern))
+        if (nrow(hit) > 0) hit$genus_group[[1]] else g
+      }
+    )
+  )
+
+# write out table
+write_tsv(sylph_profiles_metadata, "results/combined-spontaneous-oat-profiles.tsv")
 
 #################################
 # Basic summary stats
@@ -56,18 +90,44 @@ sylph_profiles_stats <- sylph_profiles_metadata %>%
     .groups = "drop"
   )
 
-# prep df for showing abundance of top species
+# prep df for showing abundance of top genera
 abundance_df_labelled <- sylph_profiles_metadata %>% 
   group_by(sample) %>% 
   arrange(desc(Sequence_abundance), .by_group = TRUE) %>% 
   mutate(
     rank_in_sample = row_number(),
-    species_label = if_else(rank_in_sample <=5, species, "Other Species")
+    
+    # robust "missing genus" flag (handles real NA + common string-y NA forms)
+    genus_missing = is.na(genus_group) |
+      str_trim(as.character(genus_group)) %in% c("", "NA", "N/A", "<NA>", "na", "n/a"),
+    
+    genus_label = case_when(
+      genus_missing        ~ "Other Genera",
+      rank_in_sample <= 5  ~ as.character(genus_group),
+      TRUE                 ~ "Other Genera"
+    )
   ) %>% 
   ungroup() %>% 
-  select(sample, oat, Sequence_abundance, species_label)
+  select(sample, oat, Sequence_abundance, genus_label, day)
 
-## plot of abundance of top species
+genus_levels <- abundance_df_labelled %>%
+  distinct(genus_label) %>%
+  pull(genus_label) %>%
+  as.character()
+
+genus_levels <- c(sort(setdiff(genus_levels, "Other Genera")), "Other Genera")
+
+abundance_df_labelled <- abundance_df_labelled %>%
+  mutate(genus_label = factor(genus_label, levels = genus_levels))
+
+oat_order <- c("oat_4", "oat_9", "oat_10", "oat_16", "oat_17", "oat_18", "oat_19", "oat_22", "oat_23", "oat_26")
+
+abundance_df_labelled <- abundance_df_labelled %>%
+  mutate(
+    oat = factor(oat, levels = oat_order)
+  )
+
+## plot of abundance of top genera
 
 # color palette prep
 
@@ -76,25 +136,48 @@ okabe_ito <- c(
   "#0072B2", "#D55E00", "#CC79A7", "#000000"
 )
 
-pal17 <- colorRampPalette(okabe_ito)(17)
+okabe_ito_palette <- colorRampPalette(okabe_ito)(22)
 
+non_other <- setdiff(genus_levels, "Other Genera")
+
+genus_colors <- c(
+  setNames(okabe_ito_palette[seq_along(non_other)], non_other),
+  "Other Genera" = "grey70"
+)
+
+# oat sample metadata
+oat_sample_metadata <- read_tsv("metadata/oat-sample-metadata.tsv")
+
+oat_order <- c("oat_4", "oat_9", "oat_10", "oat_16", "oat_17", "oat_18", "oat_19", "oat_22", "oat_23", "oat_26")
+
+
+# plot
 oat_abundance_plot <- abundance_df_labelled %>% 
-  ggplot(aes(x=sample, y=Sequence_abundance, fill=species_label)) +
+  left_join(oat_sample_metadata) %>% 
+  mutate(oat_type = paste0(oat, "\n", stringr::str_wrap(product_name, width = 23))) %>%  
+  mutate(oat_type = factor(oat_type, 
+                           levels = unique(oat_type[order(match(oat, oat_order))]))) %>% 
+  mutate(day = gsub("day", "", day)) %>% 
+  ggplot(aes(x=day, y=Sequence_abundance, fill=genus_label)) +
   geom_col() +
-  facet_wrap(~ fct_rev(oat), scales = "free_x",) +
+  facet_wrap(~ oat_type, scales = "free_x", nrow = 2,
+             labeller = as_labeller(function(x) x)) +
   theme_bw() +
   scale_x_discrete(expand=c(0,0)) +
   scale_y_continuous(expand=c(0,0)) +
-  scale_fill_manual(values = pal17) +
+  scale_fill_manual(values = genus_colors) +
+  guides(fill = guide_legend(ncol = 1)) +
   theme(axis.text.x = element_text(size=14), axis.text.y=element_text(size=14), axis.title.x=element_text(size=15), axis.title.y=element_text(size=15), plot.title=element_text(face="bold", size=16), legend.text=element_text(size=14), legend.title=element_text(size=15), strip.text=element_text(size=15), strip.background=element_rect(fill="white", color="black")) +
   labs(
-    x="Oat Sample",
+    x="Sample Day",
     y="% Sequence Abundance",
-    fill="Species",
-    title="Sequence Abundance of Top Species in Spontaneous Fermented Oat Samples"
+    fill="Genus",
+    title="Sequence Abundance of Top Genera in Spontaneously Fermented Oat Samples"
   )
 
-ggsave("figures/zymo-oat-sequencing-abundance-plot.png", oat_abundance_plot, width=12, height=7, units=c("in"))
+oat_abundance_plot
+
+ggsave("figures/zymo-oat-sequencing-abundance-plot.png", oat_abundance_plot, width=15, height=9, units=c("in"))
 
 ## plot ANI distribution of hits along with colored abundance categories
 # first categorize by high, medium, and low sequence abundance to color by
